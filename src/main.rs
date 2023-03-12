f08a16293f34b26022fe9aea6c612005b7f40f1b
Gabriel Martinez <reitaka@gmail.com>
2023-03-12 11:19:39 -0700

    Update engines cache dir on Windows
    Add an intermediate cache directory on Windows since it's placed in
    ~/AppData/Local with other things by default.

--- src/main.rs
+++ src/main.rs
@@ -65,13 +65,21 @@ struct FygDirs {
 impl FygDirs {
     fn new() -> Option<Self> {
         let base_dirs = BaseDirs::new()?;
+
+        let mut engines_cache_dir = base_dirs.cache_dir()
+            .join(FYG_DIR);
+        // Add an intermediate cache directory on Windows since it's placed in ~/AppData/Local
+        // with other things by default.
+        if cfg!(target_os = "windows") {
+            engines_cache_dir.push("cache");
+        }
+        engines_cache_dir.push("engines");
+
         Some(Self {
             engines_data_dir: base_dirs.data_dir()
                 .join(FYG_DIR)
                 .join("engines"),
-            engines_cache_dir: base_dirs.cache_dir()
-                .join(FYG_DIR)
-                .join("engines"),
+            engines_cache_dir,
         })
     }
 
